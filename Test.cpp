f1caea4a01ca8233f805220d78b602c105cb811c
Eti Reznikov <44700034+EtiReznikov@users.noreply.github.com>
2019-05-13 21:31:13 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -12,8 +12,8 @@ using namespace std;
 using namespace itertools;
 template<typename Iterable>
 string iterable_to_string(const Iterable& iterable) {
-   /* ostringstream ostr;
-    for (decltype(*iterable.begin()) i: iterable)
+   ostringstream ostr;
+   /* for (decltype(*iterable.begin()) i: iterable)
         ostr << i << " ";*/
     return ostr.str();
 }
